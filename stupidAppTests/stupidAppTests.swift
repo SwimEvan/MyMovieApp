8115dcd5fd2be82ae08323b31ce40d3824881af9
EvanSwims <evanswimming@gmail.com>
2025-01-16 07:43:18 -0600

    Initial Commit

---
+++ stupidAppTests/stupidAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  stupidAppTests.swift
+//  stupidAppTests
+//
+//  Created by EVAN MEYER on 1/16/25.
+//
+
+import Testing
+@testable import stupidApp
+
+struct stupidAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
